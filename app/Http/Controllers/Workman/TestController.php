3121a3a3c8d28c5cf1e519e18bf58fbd13b8f113
wangliuzheng <wangliuzheng@tourongjia.com>
2017-09-14 15:37:35 +0800

    llllll

--- app/Http/Controllers/Workman/TestController.php
+++ app/Http/Controllers/Workman/TestController.php
@@ -24,7 +24,22 @@ class TestController extends Controller
     }
 
     public function agency(){
-        echo 'agency';
+
+        $worker = new Worker('tcp://0.0.0.0:443');
+
+        $worker->onConnect = function ($connection){
+            $connection_baidu = new AsyncTcpConnection('tcp://www.baidu.com:443');
+            $connection_baidu->onMessage = function ($connection_baidu,$data) use ($connection){
+                $connection->send($data);
+            };
+            $connection->onMessage = function ($connection,$data) use ($connection_baidu){
+                $connection_baidu->send($data);
+            };
+            $connection_baidu->connect();
+        };
+
+        // 运行worker
+        Worker::runAll();
     }
 
 
