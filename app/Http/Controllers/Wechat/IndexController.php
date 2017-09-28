d630c11c7997328007b47be3f2230c02219b6d32
wangliuzheng <wangliuzheng@tourongjia.com>
2017-09-28 14:01:24 +0800

    log

--- app/Http/Controllers/Wechat/IndexController.php
+++ app/Http/Controllers/Wechat/IndexController.php
@@ -13,20 +13,23 @@ use \App\Model\ExceptionLog;
 class IndexController
 {
     const TOKEN = '20170925wangliuzheng';
+    public $exception_log;
+    public function __construct()
+    {
+        $this->exception_log = new  ExceptionLog();
+    }
 
-    
     //用户发给公众号的消息以及开发者需要的事件推送，将被微信转发到该方法中
     public function index(){
-        $log = new  ExceptionLog();
-        $log->type = 'test';
-        $log->content = 'weixin';
-        $log->save();
+
+        $this->exception_log->add('1：','wechat');
         //验证消息的确来自微信服务器
         $echostr = $_GET['echostr'];
         $is_from_weixin_server = $this->checkSignature();
         if($is_from_weixin_server && $echostr){
             return $echostr;
         }else{
+            $this->exception_log->add('2：','wechat');
             $this->responseMsg();
         }
         return view('errors.403');
@@ -50,6 +53,7 @@ class IndexController
         if(empty($postStr)){
             $postStr = file_get_contents("php://input");
         }
+        $this->exception_log->add('3：'.$postStr,'wechat');
         $postObj = simplexml_load_string($postStr);
         if(strtolower($postObj->MsgType) == 'event'){
             if(strtolower($postObj->Event) == 'subscribe'){
